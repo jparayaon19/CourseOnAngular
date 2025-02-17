a2c9d41d27dd92ebe0361c266a4bbc45a03bd891
Jazz Parayaoan <jparayaoan19@mail.wou.edu>
2025-02-16 18:12:28 -0800

    created the contact seller page

--- buy-and-sell/src/app/contact-page/contact-page.component.ts
+++ buy-and-sell/src/app/contact-page/contact-page.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Listing } from '../types';
+import { fakeListings } from '../fake-data';
 
 @Component({
   selector: 'app-contact-page',
@@ -7,6 +10,24 @@ import { Component } from '@angular/core';
   templateUrl: './contact-page.component.html',
   styleUrl: './contact-page.component.css'
 })
-export class ContactPageComponent {
+export class ContactPageComponent implements OnInit{
+  email: string = "";
+  message: string = "";
+  listing?: Listing;
+  
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {}
 
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find(listing => listing.id === id);
+    this.message = `Hi, I'm interested in your ${this.listing?.name.toLowerCase()}!`;
+  }
+
+  sendMessage(): void{
+    alert('Your message has been sent!');
+    this.router.navigateByUrl('/listings');
+  }
 }
